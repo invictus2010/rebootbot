969a58e0e2bb38c01d2f87318f2a36c5795be99d
Jeff <jeffrey.withington@gmail.com>
2021-12-10 17:07:06 -0500

    clean up code

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ digitalocean_droplet_id = os.environ.get("digitalocean_droplet_id")
 client = discord.Client()
 twilio_client = Client(twilio_account, twilio_token)
 
+
 def reboot_digitalocean():
     data = {"type": "reboot"}
     endpoint = f'https://api.digitalocean.com/v2/droplets/{digitalocean_droplet_id}/actions'
@@ -27,6 +28,7 @@ def reboot_digitalocean():
     else:
         print(response.status_code)
 
+
 def text_message():
     message = twilio_client.messages \
         .create(
@@ -35,6 +37,7 @@ def text_message():
             to = phone_number # Number you want to text
         )
     return message.status
+
 
 @client.event
 async def on_message(message):
@@ -47,14 +50,16 @@ async def on_message(message):
             print(author_role_ids)
     elif message.content.startswith('^ping'):
         sms_message = text_message()
-        await message.channel.send(f'Texting Invictus. Message status: {sms_message}') # Customize to your name
+        await message.channel.send(
+            f'Texting Invictus. Message status: {sms_message}'
+        )  # Customize to your name
     elif message.content.startswith('^test'):
         author_role_ids = [y.id for y in message.author.roles]
         if 788117328046719017 in author_role_ids:
             await message.channel.send('I am awake...')
         else:
             print(author_role_ids)
-    
+
 
 client.run(discord_token)
 
@@ -78,4 +83,3 @@ client.run(discord_token)
 #     # except:
 #     #     await ctx.channel.send("pong")
 #     await ctx.channel.send("pong")
-
